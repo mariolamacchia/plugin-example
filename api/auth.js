21428b2c9cd149ec619d1216c8b6c46794023cfb
Mario Lamacchia <mlamacchia@hubspot.com>
2022-02-01 11:46:51 +0000

    update

--- api/auth.js
+++ api/auth.js
@@ -2,6 +2,14 @@ const hubspot = require('@hubspot/api-client');
 
 export default function auth(req, res) {
   const hubspotClient = new hubspot.Client();
+  console.log(JSON.parse(req.body), JSON.parse(req.body).code);
+  console.log(
+    'authorization_code',
+    JSON.parse(req.body).code,
+    process.env.REDIRECT_URI,
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET
+  );
   return hubspotClient.oauth.tokensApi
     .createToken(
       'authorization_code',
