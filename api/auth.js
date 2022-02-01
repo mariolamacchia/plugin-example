7892da44b69b140a3abf9ad1aa04d8dac2e61a3f
Mario Lamacchia <mlamacchia@hubspot.com>
2022-02-01 12:20:18 +0000

    update

--- api/auth.js
+++ api/auth.js
@@ -18,11 +18,9 @@ export default function auth(req, res) {
       process.env.CLIENT_ID,
       process.env.CLIENT_SECRET
     )
-    .then((response) => {
-      console.log(response);
-      response.json();
+    .then((auth) => {
+      res.json(auth);
     })
-    .then((json) => res.json(json))
     .catch((error) => {
       console.log(error);
       res.status(500).json(error);
